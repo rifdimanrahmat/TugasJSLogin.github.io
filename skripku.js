346b2ec0b4ae60e2f3ef6b3ada1f0e7f6a348d35
rifdimanrahmat <rifdimanrahmat@gmail.com>
2023-03-19 22:14:51 +0700

    first commit

---
+++ skripku.js
@@ -0,0 +1,12 @@
+function Login() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if (username === "mimin" && password === "mimin123") {
+    alert("Selamat Datang Mimin, Anda Administrator");
+  } else if (username === "budi" && password === "budi123") {
+    alert("Selamat Datang Budi, Anda Operator");
+  } else {
+    alert("Maaf Anda Gagal Login");
+  }
+}
